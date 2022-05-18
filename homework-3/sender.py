e2f05da0f65b9bf9c1a999a2ef314b4c9de48a5f
EkaterinaBannikova <105719162+EkaterinaBannikova@users.noreply.github.com>
2022-05-19 00:00:40 +0300

    homework-3

---
+++ homework-3/sender.py
@@ -0,0 +1,21 @@
+﻿import requests
+
+name = input('Введите имя: ')
+print('Чтобы увидеть в чате справку по мессенджеру, отправьте сообщение \help')
+while True:
+    text = input('Введите сообщение: ')
+    text_lstrip = text.lstrip()
+    if text_lstrip[0] != '~':
+        response = requests.post('http://127.0.0.1:5000/send',
+                                 json={
+                                     'name': name,
+                                     'text': text
+                                 }
+                                )
+    else:
+        response = requests.post('http://127.0.0.1:5000/send',
+                                 json={
+                                     'name': '-',
+                                     'text': text_lstrip
+                                 }
+                                )
