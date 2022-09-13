32d093da00776ba5e2a9cb4867449986ece2840e
EkaterinaBannikova <105719162+EkaterinaBannikova@users.noreply.github.com>
2022-09-13 22:49:31 +0300

    Delete homework-4 directory

--- homework-4/TwoSum_algorithm3.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dicti = {nums[0]: 0}
-        for i in range(1, len(nums)):
-            d = target - nums[i]
-            if d in dicti:
-                return [dicti[d], i]
-            dicti[nums[i]] = i
-            
